ebb283cd9bceb9237c801f397c5c11453e5508dd
josephwan21 <josephbwanzj@gmail.com>
2026-01-26 22:51:26 +0800

    Minor changes to imports again

--- backend/fileService/app.py
+++ backend/fileService/app.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_cors import CORS
 
-from file_service_routes import file_blueprint
+from fileService.file_service_routes import file_blueprint
 
 app = Flask(__name__)
 app.secret_key = "only-secret-key"
